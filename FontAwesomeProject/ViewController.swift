6deeb896ebfaf4766558467c042897f8d1a1756a
James Russell Orola <jrmorola@gmail.com>
2017-07-19 21:42:31 +0800

    Initial Commit

---
+++ FontAwesomeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FontAwesomeProject
+//
+//  Created by James Russell Orola on 19/07/2017.
+//  Copyright © 2017 James Russell Orola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
